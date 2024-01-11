57ca94c5c3d1514bc0b476e05d4cd8e8b5d2ef27
PriceWise <73356785+pricemastertab@users.noreply.github.com>
2024-01-11 20:04:39 -0300

    Add files via upload
    um projeto de python, que mostra os numeros divisíveis pelo o que voce escolher

---
+++ main.py
@@ -0,0 +1,6 @@
+valor1 = int(input('digite o número inicial: '))
+valor2 = int(input('digite o número final: '))
+digite = float(input('digite o valor que os números podem ser divididos: '))
+
+for c in range(valor1, valor2, 4):
+    print(c+1)
